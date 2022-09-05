2e2c18b569a05bd84241dac3d96b8800943ddb9a
arafat <ahmed.arafat353@gmail.com>
2022-09-05 15:41:56 +0200

    add some assets

---
+++ lib/shared/asset_path.dart
@@ -0,0 +1,21 @@
+class AppAssets {
+  static const String addSquare1 = "assets/icons/add-square-1.svg";
+  static const String addSquare = "assets/icons/add-square.svg";
+  static const String apple = "assets/icons/apple.svg";
+  static const String arrowRight = "assets/icons/arrow-right.svg";
+  static const String calendar = "assets/icons/calendar.svg";
+  static const String car = "assets/icons/car.svg";
+  static const String card = "assets/icons/card-pos.svg";
+  static const String coin = "assets/icons/coin.svg";
+  static const String dollarCircle = "assets/icons/dollar-circle.svg";
+  static const String driving = "assets/icons/driving.svg";
+  static const String edit2 = "assets/icons/edit-2.svg";
+  static const String keyboardOpen = "assets/icons/keybard-open.svg";
+  static const String location = "assets/icons/location.svg";
+  static const String logout = "assets/icons/logout.svg";
+  static const String mirror = "assets/icons/mirror.svg";
+  static const String setting2 = "assets/icons/setting-2.svg";
+  static const String smartCar = "assets/icons/smartCar.svg";
+  static const String trash = "assets/icons/trash.svg";
+  static const String verify = "assets/icons/verify.svg";
+}
